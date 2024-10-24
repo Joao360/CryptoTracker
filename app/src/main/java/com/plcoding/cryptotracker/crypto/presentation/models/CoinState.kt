2cf01136adb265171787a96434767d0fe9d01336
João Graça <joaograca360@gmail.com>
2024-10-24 12:30:47 +0100

    feat: implement coin list item composable and state mappers

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinState.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinState.kt
@@ -1,6 +1,10 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
+import android.icu.text.NumberFormat
 import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.util.Locale
 
 data class CoinState(
     val id: String,
@@ -17,3 +21,28 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinState(): CoinState {
+    return CoinState(
+        id = id,
+        name = name,
+        rank = rank,
+        symbol = symbol,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
